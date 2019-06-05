f3bdb80f9d213a29eaf970f1c26aa6dd07b20954
HeroBatman <44719708+HeroBatman@users.noreply.github.com>
2019-06-05 13:06:31 +0600

    Add files via upload

---
+++ src/org/Main.java
@@ -0,0 +1,13 @@
+package org;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new MatrixFace();
+
+    }
+
+}
